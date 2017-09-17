7f6f5da0522a805e958fc625ded701d07c1dcfec
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-09-17 10:03:20 +0700

    Finish CSS for the first page

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,22 @@ import styled, { injectGlobal } from 'emotion/react'
 import Link from 'next/link'
 
 const Index = props => (
-  <div>
-    <Link href="/guest">
-      <Button>ทั่วไป</Button>
-    </Link>
-    <Link href="/member">
-      <Button>สมาชิก</Button>
-    </Link>
-  </div>
+  <CenterContainer>
+    <ButtonContainer>
+      <Link href="/guest">
+        <Button big style={{ maxWidth: 180, width: '100%' }}>
+          ทั่วไป
+        </Button>
+      </Link>
+    </ButtonContainer>
+    <ButtonContainer>
+      <Link href="/member">
+        <Button big style={{ maxWidth: 180, width: '100%' }}>
+          สมาชิก
+        </Button>
+      </Link>
+    </ButtonContainer>
+  </CenterContainer>
 )
 
 export default Index
@@ -20,12 +28,44 @@ injectGlobal`
   }
 `
 
+const CenterContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  color: rgba(0%, 0%, 0%, 0.7);
+`
+
+const ButtonContainer = styled.div`
+  flex-basis: 100%;
+  text-align: center;
+
+  &:not(:first-child) {
+    margin-top: 1em;
+  }
+
+  @media (min-width: 480px) {
+    flex-basis: initial;
+    max-width: 180px;
+    width: 100%;
+
+    &:not(:first-child) {
+      margin-top: inherit;
+      margin-left: 2em;
+    }
+  }
+`
+
 const Button = styled.button`
   background: #b1dddd;
   border: 1px solid rgba(0, 0, 0, 0.11);
   border-radius: 2em;
   padding: 0.5em 1em;
   outline: none;
+  color: rgba(0%, 0%, 0%, 0.7);
+  font-size: ${props => (props.big ? '2em' : '1em')};
 
   &:hover {
     background: #b1d6de;
